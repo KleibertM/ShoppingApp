9f41dc82b7ffefe12bc260e7120d8576a257446e
kleibert <kleibertmedina@gmail.com>
2023-05-10 22:43:45 -0500

    AddMaps

--- ShoppingApp/ShoppingApp/Nosotros.xaml.cs
+++ ShoppingApp/ShoppingApp/Nosotros.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
 using Xamarin.Forms.Xaml;
 
 namespace ShoppingApp
@@ -15,6 +16,42 @@ namespace ShoppingApp
         public Nosotros()
         {
             InitializeComponent();
+
+            Pin adrees = new Pin()
+            {
+                Type = PinType.Place,
+                Label = "Adrees",
+                Address = "Falabella Puruchuco",
+                Position = new Position(-12.039819231826888, -76.93248575327226),
+                Tag = "id_adrees",
+            };
+
+            map.Pins.Add(adrees);
+            map.MoveToRegion(MapSpan.FromCenterAndRadius(adrees.Position, Distance.FromMeters(500)));
+
+            Pin adres = new Pin()
+            {
+                Type = PinType.Place,
+                Label = "Adrees",
+                Address = "Falabella Jockey Plaza",
+                Position = new Position(-12.085504358853342, -76.97780836522223),
+                Tag = "id_adrees",
+            };
+
+            mapS.Pins.Add(adres);
+            mapS.MoveToRegion(MapSpan.FromCenterAndRadius(adres.Position, Distance.FromMeters(500)));
+
+            Pin adreess = new Pin()
+            {
+                Type = PinType.Place,
+                Label = "Adrees",
+                Address = "Av. José Pardo 130, Miraflores 15074",
+                Position = new Position(-12.118839047171111, -77.02931400242593),
+                Tag = "id_adrees",
+            };
+
+            mapM.Pins.Add(adreess);
+            mapM.MoveToRegion(MapSpan.FromCenterAndRadius(adreess.Position, Distance.FromMeters(500)));
         }
 
         private async void btnHist_Clicked(object sender, EventArgs e)
@@ -34,6 +71,23 @@ namespace ShoppingApp
         {
             App.MasterDet.IsPresented = false;
             await App.MasterDet.Detail.Navigation.PushAsync(new Sistema());
+        }
+        private async void btnSur_Clicked(object sender, EventArgs e)
+        {
+            App.MasterDet.IsPresented = false;
+            await App.MasterDet.Detail.Navigation.PushAsync(new Surco());
+        }
+
+        private async void btnAte_Clicked(object sender, EventArgs e)
+        {
+            App.MasterDet.IsPresented = false;
+            await App.MasterDet.Detail.Navigation.PushAsync(new Ate());
+        }
+
+        private async void btnMirf_Clicked(object sender, EventArgs e)
+        {
+            App.MasterDet.IsPresented = false;
+            await App.MasterDet.Detail.Navigation.PushAsync(new Miraflores());
         }
     }
 }
